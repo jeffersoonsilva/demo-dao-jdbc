b5b486200d5403d4d00041c11dd92b03d7089241
Jefferson Alves <jefferoonsilva00@gmail.com>
2025-02-04 16:55:52 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,8 @@ public class Program {
 		// TODO Auto-generated method stub
 
 		Department obj = new Department(1, "rery");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.00, obj);
+		System.out.println(seller);
 	}
 }
