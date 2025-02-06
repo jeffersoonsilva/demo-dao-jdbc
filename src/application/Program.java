9c4a39c8c8386b84939c2f35e78a46a9772bbdaa
Jefferson Alves <jefferoonsilva00@gmail.com>
2025-02-06 16:22:42 -0300

    findByid implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,12 +12,12 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Department obj = new Department(1, "rery");
 		
-		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.00, obj);
+		
+		
 		
 		SellerDao sellerDao = DaoFactory.createSellerrDao();
-		
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 	}
